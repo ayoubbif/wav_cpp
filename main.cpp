33dc53f3f8415cf446c4c3da495c3c9f9b69ac7e
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2025-01-01 14:11:20 +0100

    Init CMake exec project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class SineOscillator {
+    float frequency, amplitude;
+    public:
+        SineOscillator(float freq, float amp) : frequency(freq), amplitude(amp) {}
+};
+
+int main() {
+    std::cout << "Hello and welcome to "  << "!\n";
+    return 0;
+}
